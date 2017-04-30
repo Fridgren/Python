e93c46ffa06834ddb5874b3850454ce48380b16c
Cedric Fridgren <cedricfridgren@gmail.com>
2017-04-30 14:40:56 +0200

    import av csv

--- import_csv.py
+++ import_csv.py
@@ -1,1 +1,7 @@
+import csv
 
+exampleFile = open('/Users/Cedric/Desktop/Python/automate_online-materials/example.csv')
+exampleReader = csv.reader(exampleFile)
+
+for row in exampleReader:
+        print('Row #' + str(exampleReader.line_num) + ' ' + str(row))
