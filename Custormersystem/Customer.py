7cfc7abb8c60ffe1aa4026665128e0dba970856c
Cedric Fridgren <cedricfridgren@gmail.com>
2017-09-23 22:27:01 +0200

    Jag har lagt till efternamn/lastname

--- Custormersystem/Customer.py
+++ Custormersystem/Customer.py
@@ -1,11 +1,14 @@
 class Customer(object):
     def __init__(self, firstname, lastname, balance=0.0):
         self.firstname = firstname
-        self.lastname = firstname
+        self.lastname = lastname
         self.balance = balance
 
     def getName(self):
         return self.firstname
+
+    def getlastname(self):
+        return self.lastname
 
     def deposit(self, amount):
         self.balance += amount
@@ -17,12 +20,15 @@ class Customer(object):
         self.balance -= amount
         return self.balance
 
+    def __str__(self):
+        return (self.firstname, self.lastname, self.balance)
+
 
 def main():
-    k = Customer("Ced")
+    k = Customer("Ced", "Yest")
     print(k.getName())
-    print(k.withdraw(-10))
-    print(k.deposit(100))
+    print(k.getlastname())
+
 
 
 if __name__ == "__main__":
