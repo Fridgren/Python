87a3ca522d7ed5b36b1d368a4424e8031f7f86fe
Cedric Fridgren <cedricfridgren@gmail.com>
2017-09-24 14:13:53 +0200

    Betyg

--- Custormersystem/Customer.py
+++ Custormersystem/Customer.py
@@ -1,14 +1,19 @@
 class Customer(object):
-    def __init__(self, firstname, lastname, balance=0.0):
+    def __init__(self, firstname, lastname, age, balance=0.0):
         self.firstname = firstname
         self.lastname = lastname
+        self.age = age
         self.balance = balance
+        print("A customer object is created.")
 
-    def getName(self):
+    def get_name(self):
         return self.firstname
 
-    def getlastname(self):
+    def get_lastname(self):
         return self.lastname
+
+    def getAge(self):
+        return self.age
 
     def deposit(self, amount):
         self.balance += amount
@@ -20,14 +25,17 @@ class Customer(object):
         self.balance -= amount
         return self.balance
 
-    def __str__(self):
-        return (self.firstname, self.lastname, self.balance)
 
+    def print_details(self):
+        print("Firstame:", self.firstname)
+        print("Lastname:", self.lastname)
+        print("Age:", self.age)
+        print("Balance:", self.balance)
+        print()
 
 def main():
-    k = Customer("Ced", "Yest")
-    print(k.getName())
-    print(k.getlastname())
+    k = Customer("Ced", "Yest", 30, 1500)
+    print(k.print_details())
 
 
 
